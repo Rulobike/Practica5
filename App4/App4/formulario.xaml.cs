cc235ca26a825f8719364233b9c84a84b373a408
rulo_ <rulo_@DESKTOP-I5H46KQ>
2017-10-02 22:29:04 -0500

    Agregar archivos de proyecto.

---
+++ App4/App4/formulario.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Formulario : ContentPage
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+        private void Info(object sender, EventArgs e)
+        {
+            DisplayAlert("Informacion Del Alumno", "DATOS PERSONALES \n \n" + Nombre.Text + "\n" + APaterno.Text + "\n" + AMaterno.Text + "\n" + Direccion.Text + "\n" + Telefono.Text + "\n \n DATOS ESCOLARES \n \n" + Carrera.Text + "\n" + Semestre.Text + "\n" + Matricula.Text + "\n \n DATOS SOCIALES \n \n" + Correo.Text + "\n" + Github.Text + "", "OK");
+            Nombre.IsEnabled = false;
+            APaterno.IsEnabled = false;
+            AMaterno.IsEnabled = false;
+            Direccion.IsEnabled = false;
+            Telefono.IsEnabled = false;
+            Carrera.IsEnabled = false;
+            Semestre.IsEnabled = false;
+            Matricula.IsEnabled = false;
+            Correo.IsEnabled = false;
+            Github.IsEnabled = false;
+            
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new calculadora());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Nombre.IsEnabled = true;
+            APaterno.IsEnabled = true;
+            AMaterno.IsEnabled = true;
+            Direccion.IsEnabled = true;
+            Telefono.IsEnabled = true;
+            Carrera.IsEnabled = true;
+            Semestre.IsEnabled =true;
+            Matricula.IsEnabled = true;
+            Correo.IsEnabled = true;
+            Github.IsEnabled = true;
+        }
+    }
+}
---
+++ App4/App4/principal.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Info(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new Formulario());
+        }
+
+        private void Calculadora(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new calculadora());
+        }
+    }
+}
